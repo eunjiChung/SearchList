9f5a9dcbeea247d08d3cccca5559ab58dc33e1b8
emily <emily@kmong.com>
2021-06-19 11:51:08 +0900

    상세 페이지 & 웹뷰 생성

---
+++ SearchList/Web/DetailWebViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailWebViewController.swift
+//  SearchList
+//
+//  Created by chungeunji on 2021/06/19.
+//
+
+import UIKit
+import WebKit
+
+class DetailWebViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+
+    var url: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initView()
+    }
+
+    private func initView() {
+
+    }
+}
+
+extension DetailWebViewController {
+
+}
