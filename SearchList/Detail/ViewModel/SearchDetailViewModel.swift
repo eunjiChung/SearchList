eec57755229184da1ffd06bd8866bf3d4078bb9b
emily <emily@kmong.com>
2021-06-19 17:10:47 +0900

    모델을 가지고 있는 뷰모델 생성

---
+++ SearchList/Detail/ViewModel/SearchDetailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchDetailViewModel.swift
+//  SearchList
+//
+//  Created by chungeunji on 2021/06/19.
+//
+
+import Foundation
+
+final class SearchDetailViewModel {
+
+    var documentModel: DocumentModel?
+}
