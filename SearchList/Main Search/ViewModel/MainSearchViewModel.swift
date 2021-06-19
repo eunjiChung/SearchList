9c0ed00e1db2edb808bfbc8f58a8e46d2baabbdd
emily <emily@kmong.com>
2021-06-19 16:58:25 +0900

    api 요청 수정

--- SearchList/Main Search/ViewModel/MainSearchViewModel.swift
+++ SearchList/Main Search/ViewModel/MainSearchViewModel.swift
@@ -7,62 +7,104 @@
 
 import Foundation
 
+
 final class MainSearchViewModel {
+
+    typealias DispatchLeave = (() -> Void)?
 
     let dispatchQueue = DispatchQueue(label: "search query request", qos: .background, attributes: .concurrent)
     let dispatchGroup = DispatchGroup()
 
     var didFinishRequest: (() -> Void)?
+    var didFailRequest: ((String) -> Void)?
 
     var query: String? {
         didSet {
             requestSearch()
         }
     }
-    var sort: String?
-    var page: Int?
+    var page: Int = 1
 
-    private func requestSearch() {
-        guard let query = query else { return }
+    var sortType: SortType = .title {
+        didSet {
+            guard oldValue != sortType else { return }
+            didFinishRequest?()
+        }
+    }
+    var filterType: FilterType = .all {
+        didSet {
+            guard oldValue != filterType else { return }
+            didFinishRequest?()
+        }
+    }
 
-        let leave = { self.dispatchGroup.leave() }
-        let groups: [()] = [requestBlog(query: query, completion: leave), requestCafe(query: query, completion: leave)]
+    var blogModel: SearchResultModel?
+    var cafeModel: SearchResultModel?
+    var resultModel: [DocumentModel] = []
+
+    func requestSearch() {
+//        guard let query = query else { return }
+        let query = "아이유"
+
+        page += 1
+
+        let leave: DispatchLeave = { [weak self] in
+            self?.dispatchGroup.leave()
+        }
+        let groups: [()] = [requestBlog(query: query, completion: leave),
+                            requestCafe(query: query, completion: leave)]
         groups.forEach({ method in
             dispatchGroup.enter()
             dispatchQueue.async { method }
         })
         dispatchGroup.notify(queue: .main) {
-            self.didFinishRequest?()
+            self.generateListModel()
         }
     }
 
-    private func requestBlog(query: String, completion: (() -> Void)?) {
-        NetworkAdapter.request(target: SearchAPI.searchBlog(query: query, sort: "", page: 1)) { response in
+    private func generateListModel() {
+        if let blogModel = blogModel {
+            blogModel.documents?.forEach({ resultModel.append($0) })
+        }
+        if let cafeModel = cafeModel {
+            cafeModel.documents?.forEach({ resultModel.append($0) })
+        }
+        didFinishRequest?()
+    }
+
+    private func requestBlog(query: String, completion: DispatchLeave) {
+        NetworkAdapter.request(target: SearchAPI.searchBlog(query: query, page: page)) { response in
             do {
-                let blogResult = try JSONDecoder().decode(SearchResultModel.self, from: response.data)
-                print("❤️ blog:", blogResult)
+                self.blogModel = try JSONDecoder().decode(SearchResultModel.self, from: response.data)
+                completion?()
             } catch {
                 print("‼️", error)
+                completion?()
             }
         } error: { error in
             print("‼️", error)
+            completion?()
         } failure: { failError in
             print("‼️", failError)
+            completion?()
         }
     }
 
-    private func requestCafe(query: String, completion: (() -> Void)?) {
-        NetworkAdapter.request(target: SearchAPI.searchCafe(query: query, sort: "", page: 1)) { response in
+    private func requestCafe(query: String, completion: DispatchLeave) {
+        NetworkAdapter.request(target: SearchAPI.searchCafe(query: query, page: page)) { response in
             do {
-                let cafeResult = try JSONDecoder().decode(SearchResultModel.self, from: response.data)
-                print("❤️ cafe:", cafeResult)
+                self.cafeModel = try JSONDecoder().decode(SearchResultModel.self, from: response.data)
+                completion?()
             } catch {
                 print("‼️", error)
+                completion?()
             }
         } error: { error in
             print("‼️", error)
+            completion?()
         } failure: { failError in
             print("‼️", failError)
+            completion?()
         }
     }
 }
