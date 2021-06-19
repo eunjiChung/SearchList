8b431951fef1e1684f40d826d8e7d994d7ed74d7
emily <emily@kmong.com>
2021-06-19 15:13:03 +0900

    API에 필요한 url 추가

---
+++ SearchList/Network/SearchAPI.swift
@@ -0,0 +1,51 @@
+//
+//  URLString.swift
+//  SearchList
+//
+//  Created by chungeunji on 2021/06/19.
+//
+
+import Foundation
+import Moya
+
+struct KakaoKey {
+    static let appKey = "34e1bbfe849c78cdb250bfb5a9cedd87"
+    static let apiKey = "937d9098adc7ba01ad75b9f472e1a1c0"
+}
+
+enum SearchAPI {
+    case searchBlog(query: String, sort: String, page: Int)
+    case searchCafe(query: String, sort: String, page: Int)
+}
+
+extension SearchAPI: TargetType {
+
+    var baseURL: URL { return URL(string: "https://dapi.kakao.com")! }
+
+    var path: String {
+        switch self {
+        case .searchBlog(_, _, _):     return "/v2/search/blog"
+        case .searchCafe(_, _, _):     return "/v2/search/cafe"
+        }
+    }
+
+    var method: Moya.Method { return .get }
+
+    var task: Task {
+        switch self {
+        case .searchBlog(let query, let sort, let page), .searchCafe(let query, let sort, let page):
+            let param: [String: Any] = ["query": query, "sort": sort,
+                                        "page": page, "size": 25]
+            return .requestParameters(parameters: param, encoding: URLEncoding.default)
+        }
+    }
+
+    var sampleData: Data {
+        return Data()
+    }
+
+    var headers: [String: String]? {
+        let param = ["Authorization": "KakaoAK \(KakaoKey.apiKey)"]
+        return param
+    }
+}
