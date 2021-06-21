032ce59c7b2acdcddd5e5aa2cd44389caa888cec
emily <emily@kmong.com>
2021-06-22 01:05:52 +0900

    메인에 검색 히스토리 보여주는 뷰 추가
    - Search History View 추가
    - Delegate로 searchView와 뷰 hidden 처리, 검색 후 리스트 추가하기 조정

---
+++ SearchList/Main Search/View/CustomView/SearchHistoryView.swift
@@ -0,0 +1,87 @@
+//
+//  SearchHistoryView.swift
+//  SearchList
+//
+//  Created by chungeunji on 2021/06/22.
+//
+
+import UIKit
+
+protocol SearchHistoryDelegate {
+    func searchBegin()
+    func searchButtonClicked(by keyword: String)
+}
+
+class SearchHistoryView: UIView {
+
+    private let xibName = "SearchHistoryView"
+
+    @IBOutlet weak var tableView: UITableView!
+
+    var searchHistory: [String] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+        initView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+        initView()
+    }
+
+    private func commonInit() {
+        let view = Bundle.main.loadNibNamed(xibName, owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        addSubview(view)
+    }
+
+    private func initView() {
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowOffset = .zero
+        self.layer.shadowRadius = 5
+
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        tableView.register(UINib(nibName: HistoryTableViewCell.id, bundle: nil), forCellReuseIdentifier: HistoryTableViewCell.id)
+    }
+}
+
+extension SearchHistoryView: SearchHistoryDelegate {
+    func searchBegin() {
+        guard !searchHistory.isEmpty else { return }
+        self.isHidden = false
+    }
+
+    func searchButtonClicked(by keyword: String) {
+        searchHistory.insert(keyword, at: searchHistory.startIndex)
+        self.isHidden = true
+    }
+}
+
+extension SearchHistoryView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchHistory.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HistoryTableViewCell.id, for: indexPath) as! HistoryTableViewCell
+        cell.titleLabel.text = searchHistory[indexPath.row]
+        return cell
+    }
+}
+
+extension SearchHistoryView: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+}
+
