39e5eb98134cba4714a27cb273e8a338c8ee1fa9
emily <emily@kmong.com>
2021-06-22 17:54:16 +0900

    html 포함된 string을 일반 텍스트 string으로 파싱하는 작업 & 테스트

--- SearchList/Extension/String+.swift
+++ SearchList/Extension/String+.swift
@@ -46,6 +46,16 @@ extension String {
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXX"
         return dateFormatter.date(from: self)
     }
+
+    var removeHtml: String? {
+        do {
+            guard let data = self.data(using: String.Encoding.utf8) else { return nil }
+            let attributed = try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+            return attributed.string
+        } catch {
+            return nil
+        }
+    }
 }
 
 extension Date {
