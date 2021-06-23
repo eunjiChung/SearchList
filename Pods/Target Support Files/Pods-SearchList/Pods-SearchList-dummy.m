c64b0b65e4c6e88b038bf4ac41d684a44e62a66d
emily <emily@kmong.com>
2021-06-24 03:12:10 +0900

    Merge from develop

---
+++ Pods/Target Support Files/Pods-SearchList/Pods-SearchList-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SearchList : NSObject
+@end
+@implementation PodsDummy_Pods_SearchList
+@end
---
+++ Pods/Target Support Files/Pods-SearchListTests/Pods-SearchListTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SearchListTests : NSObject
+@end
+@implementation PodsDummy_Pods_SearchListTests
+@end
